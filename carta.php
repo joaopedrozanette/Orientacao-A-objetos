5cc8d935d8de105a9c83aa6f4c5446e2c3d33fe8
João Pedro <135861929+joaopedrozanette@users.noreply.github.com>
2024-09-22 23:18:39 -0300

    carta.php

--- carta.php
+++ carta.php
@@ -6,8 +6,6 @@
     private  $nome;
     
 
-  
-
     /**
      * Get the value of numero
      */
@@ -45,22 +43,66 @@
     }
   }
 
-  $baralhoCartas= array();
+$baralhoCartas = array();
 
-  $cartas= array("A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K");
-  $naipes= array("ouro", "copas", "espadas", "paus");
+$cartas = array("A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K");
+$naipes = array("ouro", "copas", "espadas", "paus");
 
-  for($i = 0;$i<7; $i++){
+for ($i = 0; $i < 7; $i++) {
 
     $NumeroAleatorio = $cartas[array_rand($cartas)];
-    $NomeAleatorio = $naipes[array_rand($naipes)] ; 
+    $NomeAleatorio = $naipes[array_rand($naipes)];
     $carta = new Carta;
     $carta->setNumero($NumeroAleatorio);
     $carta->setNome($NomeAleatorio);
     array_push($baralhoCartas, $carta);
-    
+}
+$cartaSorteada = array();
+$cartaSorteada = $baralhoCartas[array_rand($baralhoCartas)];
 
-  }
+echo "Jogo de adivinhação de Cartas: tente acertar qual carta foi sorteada.\n";
+
+$opcao = 1;
+
+echo "\nCartas disponiveis, acerte qual é a carta sorteada: \n\n";
+
+foreach ($baralhoCartas as $bc) {
+
+    echo "Opcão: " . $opcao  . " - " . $bc->getNumero() . " de " . $bc->getNome() . " \n";
+    $opcao++;
+}
+
+    $pontos = 100;
+
+do {
+    
+    $escolha = readline("Escolha uma opção(número de 1 a 7) ou digite 0 para desistir: "); 
+
+    if ($escolha == "0") {
+        echo "\nDESISTENTE! A carta era " . $cartaSorteada->getNumero() . " de " . $cartaSorteada->getNome() . ".\n";
+        break;}
+
+   
+    $escolha = $escolha - 1;
+
+    if (isset($baralhoCartas[$escolha])) {
+        $cartaEscolhida = $baralhoCartas[$escolha];
+        
+        if ($cartaEscolhida->getNumero() === $cartaSorteada->getNumero() and 
+            $cartaEscolhida->getNome() === $cartaSorteada->getNome()) {
+            echo "MAX WIN! Você acertou a carta escolhida: " . $cartaSorteada->getNumero() . " de " . $cartaSorteada->getNome() . "\n";
+            echo "Pontuação: " . $pontos . "\n";
+            break;
+        } else {
+            $pontos = $pontos - 10;
+            echo "Errooooou! TENTA DE NOVO.\n";
+        }
+    } else {
+        echo "Escolha inválida.\n";
+    }
+
+} while (true);
+
 
     
     
